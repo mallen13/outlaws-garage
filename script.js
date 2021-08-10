f74500d456f322bcfc2b767808d5dc2e6212a108
= <=>
2021-08-10 11:20:55 -0400

    significant style/ content updates

--- script.js
+++ script.js
@@ -1,47 +1,32 @@
-//Display popup menu if clicked
-const mobileMenu = document.querySelector('.mobileMenuBtn');
-mobileMenu.addEventListener('click', popupToggle);
-let initialPress = false;
+//Mobile Menu
+    const mobileMenu = document.querySelector('.mobileMenuBtn');
+    const popupOverlay = document.querySelector('.popupOverlay');
+    const popupBtn = document.querySelector('.popupBtn');
 
-function popupToggle(){
-  const popupMenu = document.
-  querySelector('.popupMenu');
+    //event listeners
+    popupOverlay.addEventListener('click', popupToggle);
+    popupBtn.addEventListener('click', popupToggle);
+    mobileMenu.addEventListener('click', popupToggle);
+    let isOpen = false;
 
-  if (initialPress === false){
-    popupMenu.style.display = 'block';
-    initialPress = true;
-  } else if (initialPress === true) {
-    popupMenu.style.display = 'none';
-    initialPress = false;
-  }
-  animateMenu();
-}
+    //toggle popup overlay
+    function popupToggle(){
+      const popupContainer = document.querySelector('.popupContainer');
+      const popupMenu = document.querySelector('.popupMenu');
 
-//animate menu function
-let animationUsed = false;
-const menuLn =[
-document.getElementById("menuLn1"),
-document.getElementById("menuLn2"),
-document.getElementById("menuLn3"),
-]
-
-const animateMenu = () =>{
-    mobileMenu.classList.add('animate');
-    menuToX();
-    setTimeout(function(){ mobileMenu.classList.remove('animate'); }, 1000);
-}
-
-//Change mobile menu to an x if clicked
-function menuToX(){
-  if (animationUsed === false){
-    menuLn[0].classList.add('dissappear');
-    menuLn[1].classList.add('rotateDown45');   
-    menuLn[2].classList.add('rotateUp45');
-    animationUsed = true;
-} else if (animationUsed === true){
-    menuLn[0].classList.remove('dissappear');
-    menuLn[1].classList.remove('rotateDown45');     
-    menuLn[2].classList.remove('rotateUp45');
-    animationUsed = false;
-   }
-}
+      //if mobile menu is closed
+      if (isOpen === false){
+        popupContainer.style.maxWidth = '100vw';
+        popupMenu.style.maxWidth = '100%';
+        popupMenu.style.padding = '10px';
+        isOpen = true;
+      //if mobile menu is open
+      } else if (isOpen === true) {
+        setTimeout( () => {
+          popupContainer.style.maxWidth = null;
+          popupMenu.style.padding = '0px';
+        }, 0400)
+        popupMenu.style.maxWidth = '0%';
+        isOpen = false;
+      }
+    }
